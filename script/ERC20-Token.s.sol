d1b59496efcb7b068099d7e4d625d0f4bf3a0f3e
10ap17 <panticaleksa1710@gmail.com>
2024-05-22 00:09:41 +0200

    added script

---
+++ script/ERC20-Token.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {ERC20Token} from "../src/ERC20-Token.sol";
+
+contract DeployFundMe is Script {
+    function run(uint256 _amount) external returns (ERC20Token) {
+        vm.startBroadcast();
+
+        ERC20Token token= new ERC20Token(_amount);
+        
+        vm.stopBroadcast();
+        return token;
+    }
+}
