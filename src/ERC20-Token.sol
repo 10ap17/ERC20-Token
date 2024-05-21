fd897e4a64a2e5fd1b9b6fa84e35086898053e5c
10ap17 <panticaleksa1710@gmail.com>
2024-05-22 00:05:45 +0200

    updated token contract

---
+++ src/ERC20-Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+import '../lib/openzeppelin-contracts/contracts/access/Ownable.sol';
+import '../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
+
+pragma solidity ^0.8.25;
+
+contract ERC20Token is Ownable, ERC20{
+
+    
+    constructor(uint256 _amount) ERC20("AleksaPantic", "AP") Ownable(msg.sender) {
+        _mint(owner(), _amount);
+    }
+    
+    function mint(uint256 _amount)external onlyOwner{
+        _mint(owner(), _amount);
+    }
+}
